503b911aad2b07a3002b726df329c76bd162b152
nguye374 <nguye374@sheridancollege.ca>
2021-11-02 06:33:11 -0400

    Nov 2 First start

--- App.js
+++ App.js
@@ -1,27 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, YellowBox } from 'react-native';
+import { StyleSheet, TextInput, View, Button } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('This is the original text');
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <StatusBar style="auto" />
-      <Button title="Change Text" onPress={() => setOutputText('This is a new text')}/>
+    <View style={{padding: 50}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+        <TextInput placeholder="Course Goal" style={{padding: 15, borderBottomColor: 'black', borderBottomWidth: 2, width: '80%'}}/>
+        <Button title="ADD"/>
+      </View>
+      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  Button: {
-    backgroundColor: '#aaa',
-  }
 });
