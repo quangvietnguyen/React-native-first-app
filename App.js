a7a2ae7cba922d6f052c3e189c9d72cab76caddc
nguye374 <nguye374@sheridancollege.ca>
2021-11-03 03:25:29 -0400

    a slightly tryout

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, TextInput, View, Button } from 'react-native';
+import { StyleSheet, TextInput, View, Button, Text } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('');
+  const printText = (event) => {
+    setText(event.target.value);
+    console.log(event);
+  }
   return (
     <View style={{padding: 50}}>
       <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
         <TextInput placeholder="Course Goal" style={{padding: 15, borderBottomColor: 'black', borderBottomWidth: 2, width: '80%'}}/>
-        <Button title="ADD"/>
+        <Button title="ADD" onPress={printText}/>
+        <Text>{text}</Text>
       </View>
       <View></View>
     </View>
