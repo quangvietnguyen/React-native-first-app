2c57d19b2b862de25a32ec7f30ff53ea296aa986
nguye374 <nguye374@sheridancollege.ca>
2021-11-03 22:21:19 -0400

    Nov 3 flex box is a default in React Native

--- App.js
+++ App.js
@@ -27,36 +27,48 @@ export default function App() {
   // );
 
   return (
-    <View style={{ padding: 50 }}>
+    <View
+      style={{
+        padding: 50,
+        flexDirection: "row",
+        width: "80%",
+        height: 300,
+        justifyContent: "space-between",
+        alignItems: "stretch",
+      }}
+    >
       <View
         style={{
-          backgroundColor: 'red',
-          width: 100,
-          height: 100,
-          justifyContent: 'center',
-          alignItems: 'center'
+          backgroundColor: "red",
+          // width: 100,
+          // height: 100,
+          flex: 1, //now this child View component can get as much as space of the parent it can
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
         <Text>1</Text>
       </View>
       <View
         style={{
-          backgroundColor: 'blue',
-          width: 100,
-          height: 100,
-          justifyContent: 'center',
-          alignItems: 'center'
+          backgroundColor: "blue",
+          // width: 100,
+          // height: 100,
+          flex: 1, // if flex to 2, it will take double space from the first one.
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
         <Text>2</Text>
       </View>
       <View
         style={{
-          backgroundColor: 'green',
-          width: 100,
-          height: 100,
-          justifyContent: 'center',
-          alignItems: 'center'
+          backgroundColor: "green",
+          // width: 100,
+          // height: 100,
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
         <Text>3</Text>
