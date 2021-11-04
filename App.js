cb76e4ff5b1b99e6700ed0240bd1cf694731a967
nguye374 <nguye374@sheridancollege.ca>
2021-11-03 21:32:52 -0400

    Nov 3 React-native-flex-box

--- App.js
+++ App.js
@@ -1,24 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, TextInput, View, Button, Text } from 'react-native';
+import React from "react";
+import { StyleSheet, TextInput, View, Button, Text } from "react-native";
 
 export default function App() {
-  const [text, setText] = useState('');
-  const printText = (event) => {
-    setText(event.target.value);
-    console.log(event);
-  }
+  // return (
+  //   <View style={{ padding: 50 }}>
+  //     <View
+  //       style={{
+  //         flexDirection: "row",
+  //         justifyContent: "space-between",
+  //         alignItems: "center",
+  //       }}
+  //     >
+  //       <TextInput
+  //         placeholder="Course Goal"
+  //         style={{
+  //           padding: 15,
+  //           borderBottomColor: "black",
+  //           borderBottomWidth: 2,
+  //           width: "80%",
+  //         }}
+  //       />
+  //       <Button title="ADD" />
+  //     </View>
+  //     <View></View>
+  //   </View>
+  // );
+
   return (
-    <View style={{padding: 50}}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-        <TextInput placeholder="Course Goal" style={{padding: 15, borderBottomColor: 'black', borderBottomWidth: 2, width: '80%'}}/>
-        <Button title="ADD" onPress={printText}/>
-        <Text>{text}</Text>
+    <View style={{ padding: 50 }}>
+      <View
+        style={{
+          backgroundColor: 'red',
+          width: 100,
+          height: 100,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>1</Text>
       </View>
-      <View></View>
+      <View
+        style={{
+          backgroundColor: 'blue',
+          width: 100,
+          height: 100,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: 'green',
+          width: 100,
+          height: 100,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>3</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-});
+const styles = StyleSheet.create({});
